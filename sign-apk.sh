6b81be6227eb70ee488c0a852c865ac1598a2d33
Howard Liberty <liberty@anymemo.org>
2018-03-27 18:00:47 -0700

    Fix application id issues

--- sign-apk.sh
+++ sign-apk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" liberty-android-key
-zipalign -v 4 "app/build/outputs/apk/AnyMemo-$flavor-release-unsigned.apk" "app/build/outputs/apk/AnyMemo-$flavor-release.apk"
+jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/release/app-release-unsigned.apk" liberty-android-key
+zipalign -v 4 "app/build/outputs/apk/release/app-release-unsigned.apk" "app/build/outputs/apk/release/FreeOTPPlus-release.apk"
