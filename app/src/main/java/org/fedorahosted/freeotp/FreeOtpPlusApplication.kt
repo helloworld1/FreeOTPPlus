11e759ab719d1f1f9cbb64fd30ad97fd38472828
Howard Liberty <liberty@anymemo.org>
2019-05-19 11:56:30 -0700

    Add dark theme theme support. Provide option to manually switch.

--- app/src/main/java/org/fedorahosted/freeotp/FreeOtpPlusApplication.kt
+++ app/src/main/java/org/fedorahosted/freeotp/FreeOtpPlusApplication.kt
@@ -2,6 +2,7 @@ package org.fedorahosted.freeotp
 
 import android.app.Activity
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -11,12 +12,18 @@ import org.fedorahosted.freeotp.module.DaggerAppComponent
 import javax.inject.Inject
 
 class FreeOtpPlusApplication: Application(), HasActivityInjector, HasSupportFragmentInjector {
-    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>;
-    @Inject lateinit var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>;
+    @Inject lateinit var dispatchingActivityInjector: DispatchingAndroidInjector<Activity>
+    @Inject lateinit var dispatchingFragmentInjector: DispatchingAndroidInjector<Fragment>
+    @Inject lateinit var settings: Settings
 
     override fun onCreate() {
         super.onCreate()
         DaggerAppComponent.factory().create(this).inject(this)
+        if (settings.darkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        }
     }
 
     override fun activityInjector(): AndroidInjector<Activity> {
