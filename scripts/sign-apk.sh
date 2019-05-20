98bd58f260b3f95a0710e0030a7c6c8bac68aae9
Howard Liberty <liberty@anymemo.org>
2019-05-20 11:09:55 -0700

    Fix jar signing script

--- scripts/sign-apk.sh
+++ scripts/sign-apk.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/app-release-unsigned.apk" liberty-android-key
-zipalign -v 4 "app/build/outputs/apk/app-release-unsigned.apk" "app/build/outputs/apk/FreeOtpPlus-release.apk"
+APK_DIR="app/build/outputs/apk/release"
+UNSIGNED_APK_FILE_NAME="app-release-unsigned.apk"
+SIGNED_APK_FILE_NAME="FreeOtpPlus-release.apk"
+
+jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "$APK_DIR/$UNSIGNED_APK_FILE_NAME" liberty-android-key
+zipalign -v 4 "$APK_DIR/$UNSIGNED_APK_FILE_NAME" "$APK_DIR/$SIGNED_APK_FILE_NAME"
