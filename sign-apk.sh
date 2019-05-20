c5b3da26a6c90403ffa0bb8e57882d52c39ce69c
Howard Liberty <liberty@anymemo.org>
2019-05-20 10:57:12 -0700

    Clean up unused files

--- sign-apk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-jarsigner -verbose -digestalg SHA1 -sigalg MD5withRSA -keystore ~/Documents/keys/liberty-android-release.keystore "app/build/outputs/apk/release/app-release-unsigned.apk" liberty-android-key
-zipalign -v 4 "app/build/outputs/apk/release/app-release-unsigned.apk" "app/build/outputs/apk/release/FreeOTPPlus-release.apk"
