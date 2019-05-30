edefd9084dc30643ed1ba40e25b19806a5021e47
Howard Liberty <liberty@anymemo.org>
2019-05-29 21:55:24 -0700

    Use Kotlin DSL to compile the project

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        versionCode = 5
+        versionName = "1.4" 
+        minSdkVersion(19)
+        targetSdkVersion(28)
+        applicationId = "org.liberty.android.freeotpplus"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+            resValue("string", "app_name", "FreeOTP+ Debug")
+        }
+        getByName("release") {
+            resValue("string", "app_name", "FreeOTP+")
+        }
+    }
+    dataBinding {
+        isEnabled = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("com.google.android.material:material:1.1.0-alpha06")
+    implementation("com.google.zxing:core:3.3.0")
+    implementation("com.google.code.gson:gson:2.8.2")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.google.android.material:material:1.0.0")
+    implementation("androidx.core:core-ktx:1.0.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.1.1")
+    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
+    implementation("com.google.dagger:dagger:2.22.1")
+    implementation("com.google.dagger:dagger-android:2.22.1")
+    implementation("com.google.dagger:dagger-android-support:2.22.1")
+    kapt("com.google.dagger:dagger-compiler:2.22.1")
+    kapt("com.google.dagger:dagger-android-processor:2.22.1")
+    kapt("com.google.dagger:dagger-android-support:2.22.1")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.kotlin_version = '1.3.31'
     repositories {
         jcenter()
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.4.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
     }
 }
 
