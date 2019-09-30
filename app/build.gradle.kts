62a5d5f78fe5892ffa4c35a1f009f5c60792e0e6
Howard Liberty <liberty@anymemo.org>
2019-09-29 21:53:32 -0700

    Add Java 1.8 compatibility (Required by camerax)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,10 @@ android {
     dataBinding {
         isEnabled = true
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
 dependencies {
