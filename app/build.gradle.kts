8a7aadde519a2b2389c55c4e8c8f1bb6409bd7ce
Howard Liberty <liberty@anymemo.org>
2019-11-27 20:57:07 -0800

    Bump version to 1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 android {
     compileSdkVersion(28)
     defaultConfig {
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
         minSdkVersion(21)
         targetSdkVersion(28)
         applicationId = "org.liberty.android.freeotpplus"
