bd62751dd3397f165a3ac036d40ca304c84a8987
Howard Liberty <liberty@anymemo.org>
2021-06-07 21:56:37 -0700

    Upgrade to kotlin 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.1")
-        classpath(kotlin("gradle-plugin", version = "1.4.32"))
+        classpath(kotlin("gradle-plugin", version = "1.5.10"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.35")
     }
 }
