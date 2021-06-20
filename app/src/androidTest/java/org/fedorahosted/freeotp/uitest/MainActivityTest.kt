9d19241ec0946d7599f212df63f2c6955786dd82
Howard Liberty <liberty@anymemo.org>
2021-06-20 15:02:20 -0700

    Add tests for adding token

--- app/src/androidTest/java/org/fedorahosted/freeotp/uitest/MainActivityTest.kt
+++ app/src/androidTest/java/org/fedorahosted/freeotp/uitest/MainActivityTest.kt
@@ -1,7 +1,22 @@
 package org.fedorahosted.freeotp.uitest
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import kotlinx.coroutines.runBlocking
+import org.fedorahosted.freeotp.R
 import org.fedorahosted.freeotp.ui.MainActivity
 import org.junit.Rule
 import org.junit.Test
@@ -13,6 +28,30 @@ class MainActivityTest {
     var activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun test() {
+    fun testInsertToken() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext);
+        onView(withText(R.string.add_token))
+            .perform(click())
+        onView(withId(R.id.issuer))
+            .perform(typeText("issuer1"), closeSoftKeyboard())
+        onView(withId(R.id.label))
+            .perform(typeText("account1"), closeSoftKeyboard())
+        onView(withId(R.id.secret))
+            .perform(typeText("abcd12345"))
+        onView(withId(R.id.add))
+            .perform(click())
+        onView(withId(R.id.token_list))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, scrollTo()))
+            .check(matches(hasDescendant(withText("issuer1"))))
+            .check(matches(hasDescendant(withText("account1"))))
+    }
+
+    private fun populateTestData() {
+        activityRule.scenario.onActivity { activity ->
+            runBlocking {
+                activity.otpTokenDatabase.otpTokenDao().insert(TestData.OTP_HOTP_TOKEN_1)
+                activity.otpTokenDatabase.otpTokenDao().insert(TestData.OTP_TOTP_TOKEN_2)
+            }
+        }
     }
 }
---
+++ app/src/androidTest/java/org/fedorahosted/freeotp/uitest/TestData.kt
@@ -0,0 +1,21 @@
+package org.fedorahosted.freeotp.uitest
+
+import android.net.Uri
+import org.fedorahosted.freeotp.data.OtpTokenFactory
+
+object TestData {
+    val OTP_HOTP_TOKEN_1 = OtpTokenFactory.createFromUri(
+        Uri.parse("otpauth://hotp/github.com:github%20account%201?secret=abcd12345&algorithm=SHA256&digits=6&period=30&lock=false&counter=0"))
+        .copy(
+            id = 1,
+            ordinal = 1
+        )
+
+    val OTP_TOTP_TOKEN_2 = OtpTokenFactory.createFromUri(
+        Uri.parse("otpauth://totp/microsoft.com:microsoft%20account%201?secret=bcde23456&algorithm=SHA512&digits=6&period=30&lock=false"))
+        .copy(
+            id = 2,
+            ordinal = 2
+        )
+
+}
