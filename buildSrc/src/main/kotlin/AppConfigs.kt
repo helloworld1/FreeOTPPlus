3fce55243c0b64962776166962699a264dc03fc0
Howard Liberty <liberty@anymemo.org>
2021-09-05 22:32:53 -0700

    Bump version to 2.4

--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -2,6 +2,6 @@ object AppConfigs {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 30
-    const val VERSION_CODE = 14
-    const val VERSION_NAME = "2.3"
+    const val VERSION_CODE = 15
+    const val VERSION_NAME = "2.4"
 }
