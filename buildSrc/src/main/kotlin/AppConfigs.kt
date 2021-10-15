f375dfd7d7ce2c3fa14852150b4916f3e9b59b4c
Howard Liberty <liberty@anymemo.org>
2021-10-15 10:32:57 -0700

    Bump version to 2.6

--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -2,6 +2,6 @@ object AppConfigs {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 21
     const val TARGET_SDK_VERSION = 30
-    const val VERSION_CODE = 16
-    const val VERSION_NAME = "2.5"
+    const val VERSION_CODE = 17
+    const val VERSION_NAME = "2.6"
 }
