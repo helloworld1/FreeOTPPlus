06317d2343d16132a5a559bed837c76f1e2a4a7e
Howard Liberty <liberty@anymemo.org>
2022-03-12 20:18:03 -0800

    Update kotlin, AGPU and Android library dependency versions

--- buildSrc/src/main/kotlin/AppConfigs.kt
+++ buildSrc/src/main/kotlin/AppConfigs.kt
@@ -1,7 +1,7 @@
 object AppConfigs {
-    const val COMPILE_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 31
     const val MIN_SDK_VERSION = 21
-    const val TARGET_SDK_VERSION = 30
+    const val TARGET_SDK_VERSION = 31
     const val VERSION_CODE = 18
     const val VERSION_NAME = "2.7"
 }
