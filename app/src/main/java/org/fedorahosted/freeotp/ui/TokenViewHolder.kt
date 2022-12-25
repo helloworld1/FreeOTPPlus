5b8e4eab57594f0a47837324592b6919a499af82
Howard Liberty <liberty@anymemo.org>
2022-12-25 12:41:29 -0800

    Add util to share to other apps

--- app/src/main/java/org/fedorahosted/freeotp/ui/TokenViewHolder.kt
+++ app/src/main/java/org/fedorahosted/freeotp/ui/TokenViewHolder.kt
@@ -2,10 +2,12 @@ package org.fedorahosted.freeotp.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import org.fedorahosted.freeotp.R
 import org.fedorahosted.freeotp.data.OtpToken
+import org.fedorahosted.freeotp.data.OtpTokenFactory
 import org.fedorahosted.freeotp.token.TokenLayout
 
 class TokenViewHolder(private val activity: Activity,
@@ -26,6 +28,11 @@ class TokenViewHolder(private val activity: Activity,
                 val i = Intent(tokenLayout.context, DeleteActivity::class.java)
                 i.putExtra(DeleteActivity.EXTRA_TOKEN_ID, token.id)
                 activity.startActivity(i)
+            }
+
+            R.id.action_share -> {
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse(OtpTokenFactory.toUri(token).toString()))
+                activity.startActivity(Intent.createChooser(i, null))
             }
 
             R.id.action_share_qr_code -> {
